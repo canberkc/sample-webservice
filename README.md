32d49ffa5d134738d16ab333abe9885f87edb98e
canberk <questionscb@gmail.com>
2019-02-24 22:42:57 +0100

    Init project with sample rest implementation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sample-webservice
-Old school soap web service sample
+
+Sample project to describe rest and soap based web services
+
---
+++ src/main/java/com/devlabs/samples/rest/RestResources.java
@@ -0,0 +1,13 @@
+package com.devlabs.samples.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures a JAX-RS endpoint.
+ */
+
+@ApplicationPath("resources")
+public class RestResources extends Application {
+
+}
